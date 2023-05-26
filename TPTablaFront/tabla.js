89d2695fa5ad669c0dd77e2f3403a3ecf5ea32c1
Marina Erasun <marinaerasun14@gmail.com>
2023-05-26 11:56:32 -0300

    Tabla

--- TPTablaFront/tabla.js
+++ TPTablaFront/tabla.js
@@ -1,10 +1,33 @@
 import { users } from "./user.js";
 const user = JSON.parse(users);
-const list = document.querySelector("ul");
+const tabla = document.getElementById("tabla");
 
 user.forEach(el => {
-  const usr = `
-    <li>${el.name.first} ${el.name.last} / ${el.location.street.name} ${el.location.street.number} /<img src=${el.picture.thumbnail}> </li>
-    `;
-  list.innerHTML += usr;
+  const fila = document.createElement('tr');
+
+  const celdaNombre = document.createElement('td');
+  celdaNombre.textContent = el.name.first;
+  fila.appendChild(celdaNombre);
+
+  const celdaApellido = document.createElement('td');
+  celdaApellido.textContent = el.name.last;
+  fila.appendChild(celdaApellido);
+
+  const celdaDireccionNombre = document.createElement('td');
+  celdaDireccionNombre.textContent = el.location.street.name;
+  fila.appendChild(celdaDireccionNombre);
+
+  const celdaDireccionNumero = document.createElement('td');
+  celdaDireccionNumero.textContent = el.location.street.number;
+  fila.appendChild(celdaDireccionNumero);
+
+  const celdaImagen = document.createElement('td');
+  const imagen = document.createElement('img');
+  imagen.src = el.picture.thumbnail;
+  celdaImagen.appendChild(imagen);
+  fila.appendChild(celdaImagen);
+  
+  tabla.appendChild(fila);
+    
 });
+
